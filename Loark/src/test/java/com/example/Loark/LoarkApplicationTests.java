0d49e2da0abeb0d5c0fad34d668ae602477890da
hyun3138 <namhyun3418@gmail.com>
2025-08-06 09:41:49 +0900

    Basic Settings

---
+++ Loark/src/test/java/com/example/Loark/LoarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Loark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
