059f7a62ad89d469e186276547a12c14623444fc
Taeri <35729214+lola161385@users.noreply.github.com>
2025-08-26 13:43:23 +0900

    Update WebConfig.java

--- Loark/src/main/java/com/example/Loark/Config/WebConfig.java
+++ Loark/src/main/java/com/example/Loark/Config/WebConfig.java
@@ -11,6 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
                 .allowedOrigins("https://loark.kr") // 로컬 프론트엔드 주소 허용
+                .allowedOrigins(
+                         "https://loark.kr",
+                         "https://www.loark.kr",
+                         "http://localhost:5173", // Vite 개발 서버
+                         "http://localhost:3000"  // CRA 개발 서버
+                 ) // 허용할 프론트엔드 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메소드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 등 자격 증명 허용
