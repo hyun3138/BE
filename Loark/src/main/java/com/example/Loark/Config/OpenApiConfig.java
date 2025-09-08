93a5932d7b63acf7ae2af799275d2a0ded4e9c14
lola161385 <lola161385@naver.com>
2025-09-08 19:46:12 +0900

    HOTFIX: swagger error fix & save-main

---
+++ Loark/src/main/java/com/example/Loark/Config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.example.Loark.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server server = new Server();
+        server.setUrl("https://api.loark.kr");
+        server.setDescription("Default Server URL");
+        return new OpenAPI().servers(List.of(server));
+    }
+}
