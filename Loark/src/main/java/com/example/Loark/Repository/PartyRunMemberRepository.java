78407a014b3c160a3336a46927f7035e9f5d81d4
이남현 <namhyun3418@gmail.com>
2025-09-09 17:24:35 +0900

    ee

--- Loark/src/main/java/com/example/Loark/Repository/PartyRunMemberRepository.java
+++ Loark/src/main/java/com/example/Loark/Repository/PartyRunMemberRepository.java
@@ -1,10 +1,14 @@
 package com.example.Loark.Repository;
 
+import com.example.Loark.Entity.PartyRun;
 import com.example.Loark.Entity.PartyRunMember;
 import com.example.Loark.Entity.PartyRunMemberId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PartyRunMemberRepository extends JpaRepository<PartyRunMember, PartyRunMemberId> {
+    void deleteAllByPartyRunIn(List<PartyRun> partyRuns);
 }
